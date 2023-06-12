73ef427551b94fd6c00166f612e611d3a7aa3c49
joannaatento <06802182@dwc-legazpi.edu>
2023-06-13 00:58:51 +0800

    updated

--- capstone1.sql
+++ capstone1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 10, 2023 at 06:16 PM
+-- Generation Time: Jun 12, 2023 at 06:58 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -32,6 +32,7 @@ CREATE TABLE `admins` (
   `type` varchar(1) NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
+  `role` int(5) NOT NULL,
   `password` varchar(250) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -39,13 +40,10 @@ CREATE TABLE `admins` (
 -- Dumping data for table `admins`
 --
 
-INSERT INTO `admins` (`admin_id`, `type`, `username`, `email`, `password`) VALUES
-(1, '0', 'Nurse Edita', 'edita@dwc-legazpi.edu', '$2y$10$fcGjE/6lxScs57Vbgu9HV.ZsKa4Ky7NUj.NSUZPgfromKvYz2UnhK'),
-(2, '0', 'Nurse Edith', 'edith@dwc-legazpi.edu', '$2y$10$m7dgLE2yYPQrXXDU8tP4KuY8kuVpKr8/zVANbCPczSNEGUHueGfLu'),
-(3, '0', 'Nurse Chin', 'chin@dwc-legazpi.edu', '$2y$10$bv0gkXjyFoxG0o6lmBdrrO1x6LQpAh3hyW7UdGJJmXvRltYm7BiM.'),
-(4, '0', 'Nurse Cam', 'camille@dwc-legazpi.edu', '$2y$10$1fVZff89V4/Wsq5/kBVn2uel.I9WTVq5DwnVKTwu360nDC8T24d6S'),
-(5, '1', 'Nurse Joanna', 'joanna@dwc-legazpi.edu', '$2y$10$xWb/aCptUtQjMm86mgozuepwMmVW8amTeV.8yQ4/Cj96cNiKzpPbq'),
-(6, '1', 'Nurse Troy', 'troy@dwc-legazpi.edu', '$2y$10$ed.DgD6A25oN4psDEMU7X.HPKTB1xNaCt0RUbsCd4ZYMrzICQzrvS');
+INSERT INTO `admins` (`admin_id`, `type`, `username`, `email`, `role`, `password`) VALUES
+(9, '', 'Nurse GSJHS', 'gsjhs@dwc-legazpi.edu', 1, '$2y$10$GifhpjE5vxy1N8BggO6EFuEIzoPHR8QXKBUkXO8G2HKdfzjcuBAlq'),
+(10, '', 'Nurse SHS', 'shs@dwc-legazpi.edu', 2, '$2y$10$P2sGF1KR.EILlLMSBA.eYu7kXEPcmMxyXKI8n2Sgqd7.sesXYcVSu'),
+(11, '', 'Nurse College', 'college@dwc-legazpi.edu', 3, '$2y$10$vgjsovnMxuz4nNvShLSKeuNSVpfsOswZ56y6yQFsO.MwH4f8BZrVC');
 
 -- --------------------------------------------------------
 
@@ -60,17 +58,21 @@ CREATE TABLE `dental` (
   `name` varchar(50) NOT NULL,
   `dental_service` varchar(50) NOT NULL,
   `c_enrolled` varchar(50) NOT NULL,
+  `gradecourseyear` varchar(200) NOT NULL,
+  `c_employee` varchar(100) NOT NULL,
   `message` varchar(1000) NOT NULL,
   `date_created` datetime NOT NULL,
-  `is_read` int(11) NOT NULL DEFAULT 0
+  `is_read` int(11) NOT NULL DEFAULT 0,
+  `is_deleted_on_website` tinyint(1) DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `dental`
 --
 
-INSERT INTO `dental` (`dental_id`, `user_id`, `idnumber`, `name`, `dental_service`, `c_enrolled`, `message`, `date_created`, `is_read`) VALUES
-(31, 20, 123456, 'Joanna Mae Atento', 'Cleaning', 'Grade School & Grade School & Junior High School', 'Pa sched po ako, Monday 10:30 A.M', '2023-06-09 10:25:00', 0);
+INSERT INTO `dental` (`dental_id`, `user_id`, `idnumber`, `name`, `dental_service`, `c_enrolled`, `gradecourseyear`, `c_employee`, `message`, `date_created`, `is_read`, `is_deleted_on_website`) VALUES
+(68, 20, 6802182, 'Joanna Mae Atento', 'Cleaning', 'Grade School & Grade School & Junior High School', 'Grade 7 Saint', '', 'Pa sched po ako, Monday 10:30 A.M', '2023-06-12 10:16:00', 1, 1),
+(69, 20, 6802182, 'Joanna Mae Atento', 'Cleaning', 'Grade School & Grade School & Junior High School', 'Grade 7 Saint', '', 'Pa sched po ako, Monday 10:30 A.M', '2023-06-12 11:14:00', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -87,16 +89,6 @@ CREATE TABLE `dentalapp` (
   `cenrolled` varchar(50) NOT NULL,
   `date_time` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `dentalapp`
---
-
-INSERT INTO `dentalapp` (`dentalapp_id`, `admin_id`, `idnumber`, `fullname`, `role`, `cenrolled`, `date_time`) VALUES
-(4, 0, 123456, 'Joanna Mae Atento', 'Student', 'BSIT-3', '2023-06-23 10:30:00'),
-(5, 0, 123456, 'Joanna Mae Atento', 'Student', 'BSIT-3', '2023-06-08 10:30:00'),
-(6, 0, 123456, 'Mae Atento', 'Student', 'BSIT-3', '2023-05-31 10:30:00'),
-(7, 5, 1234567, 'Mae Cruz', 'Student', 'BSIT-3', '2023-06-16 10:30:00');
 
 -- --------------------------------------------------------
 
@@ -151,7 +143,36 @@ CREATE TABLE `healthrecord` (
 
 INSERT INTO `healthrecord` (`health_id`, `user_id`, `image`, `fullname`, `idnumber`, `personalcpnum`, `age`, `birthday`, `gender`, `address`, `role`, `gradecourse`, `leveleduc`, `fathername`, `cfather`, `mothername`, `cmother`, `polio`, `measles`, `tb`, `seizure_epilepsy`, `tetanus`, `mumps`, `hepatits`, `bleeding_tendencies`, `chicken_pox`, `asthma`, `fainting_spells`, `eye_disorder`, `heart`, `illness`, `allergyfood`, `allergymed`, `allow_not`, `medications`, `nameperson`, `personcp`, `relationship`) VALUES
 (73, 20, 'me.jpg', 'Joanna Mae Atento', 6802182, '09156005165', 21, '2001-12-15', 'Female', 'Guinobatan', 'Student', 'BSIT-3', 'College', 'Jojo Atento', '09134374637', 'Rowena Atento', '091476548368', '', '', '', '', '', '', '', '', '', '', '', 'eye_disorder', 'N/A', 'N/A', '', '', 'YES', 'NO', 'Edita Dela Cruz', '091564957760', 'Grandmother'),
-(74, 21, 'WIN_20230311_16_23_26_Pro.jpg', 'Employee Juan', 12345678, '09756578679', 22, '2000-12-15', 'Male', 'Polangui', 'Employee', 'Professor', 'Employee', 'Jose Rizal', '09145885479', 'Rowena Atento', '091476548368', '', '', '', '', '', '', '', '', '', '', 'fainting_spells', '', 'N/A', 'N/A', '', '', 'YES', 'NO', 'Tandang Sora', '091475865879', 'Grandmother');
+(74, 21, 'WIN_20230311_16_23_26_Pro.jpg', 'Employee Juan', 12345678, '09756578679', 22, '2000-12-15', 'Male', 'Polangui', 'Employee', 'Professor', 'Employee', 'Jose Rizal', '09145885479', 'Rowena Atento', '091476548368', '', '', '', '', '', '', '', '', '', '', 'fainting_spells', '', 'N/A', 'N/A', '', '', 'YES', 'NO', 'Tandang Sora', '091475865879', 'Grandmother'),
+(75, 23, 'WIN_20230311_16_23_21_Pro.jpg', 'Charity Corpuz', 898989, '09275467589', 20, '2002-12-15', 'Female', 'Guinobatan', 'Student', 'BLISS-3', 'College', 'Marco Corpuz', '094375568768', 'Liezel Corpuz', '093477578996', 'polio', 'measles', '', '', '', '', '', '', '', '', '', 'eye_disorder', 'N/A', 'N/A', '', '', 'YES', 'NO', 'Domitilia Corpu', '092377435826', 'Grandmother');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patientrecord`
+--
+
+CREATE TABLE `patientrecord` (
+  `p_id` int(11) NOT NULL,
+  `admin_id` int(11) NOT NULL,
+  `idnumber` int(11) NOT NULL,
+  `fullname` varchar(200) NOT NULL,
+  `gradesection` varchar(200) NOT NULL,
+  `vitalsigns` varchar(200) NOT NULL,
+  `diagnosis` varchar(500) NOT NULL,
+  `date_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `patientrecord`
+--
+
+INSERT INTO `patientrecord` (`p_id`, `admin_id`, `idnumber`, `fullname`, `gradesection`, `vitalsigns`, `diagnosis`, `date_time`) VALUES
+(5, 0, 0, 'Joanna Mae Atento', 'Grade 7 Saint Paul', '21', 'Advil', '2023-06-13 12:37:00'),
+(6, 0, 0, 'Joanna Mae Atento', 'Grade 7 Saint', '9', 'Advil', '2023-06-13 12:39:00'),
+(7, 9, 6802182, 'Joanna Mae Atento', 'Grade 7 Saint Paul', '12A', 'Advil', '2023-06-13 12:47:00'),
+(8, 9, 1234567, 'Camila Salvador', 'Grade 7 Saint', '3853563', 'Biogesic', '2023-06-13 12:50:00'),
+(9, 9, 1234567, 'Camila Salvador', 'Grade 7 Saint', '1234557', 'Buscopan', '2023-06-13 12:51:00');
 
 -- --------------------------------------------------------
 
@@ -184,6 +205,7 @@ CREATE TABLE `users` (
   `user_id` int(11) NOT NULL,
   `type` varchar(1) NOT NULL,
   `fullname` varchar(50) NOT NULL,
+  `idnumber` int(50) NOT NULL,
   `email` varchar(50) NOT NULL,
   `password` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -192,11 +214,9 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`user_id`, `type`, `fullname`, `email`, `password`) VALUES
-(1, '1', 'test', 'test@dwc-legazpi.edu', 'test@dwc-legazpi.edu'),
-(20, '1', 'Joanna Mae Atento', '06802182@dwc-legazpi.edu', '$2y$10$Gl9YlutZ.ZQ.Atbhn4qnqOmK/tVfv4iQHKCKleUx9wfp7VjPSAnVO'),
-(21, '1', 'Employee Juan', 'employeejuan@dwc-legazpi.edu', '$2y$10$muer4YTynndgh1ZhSh1FPetejl3aNOq3TCUs7HePeYw8H/Fq2M1ha'),
-(22, '1', 'Nurse Cha', 'charot@dwc-legazpi.edu', '$2y$10$7U1zD1neafVsgN/bJUGuR.IdSUBZIwrP.raB50kvyg2xezfhefiue');
+INSERT INTO `users` (`user_id`, `type`, `fullname`, `idnumber`, `email`, `password`) VALUES
+(24, '1', 'Joanna Mae Atento', 6802182, '06666@dwc-legazpi.edu', '$2y$10$oSSy892tBzn56X5n9JoiFemFQrHcdJcKIUCW8L5aYZgmmaF.9Xm4S'),
+(25, '1', 'Camila Salvador', 1234567, 'camille@dwc-legazpi.edu', '$2y$10$Mt817fi.GicLwj8BLTFHg.AR7VvE14ra4eDj8akuBiv1HNlqx3pm.');
 
 --
 -- Indexes for dumped tables
@@ -227,6 +247,12 @@ ALTER TABLE `healthrecord`
   ADD PRIMARY KEY (`health_id`);
 
 --
+-- Indexes for table `patientrecord`
+--
+ALTER TABLE `patientrecord`
+  ADD PRIMARY KEY (`p_id`);
+
+--
 -- Indexes for table `status`
 --
 ALTER TABLE `status`
@@ -246,13 +272,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `admins`
 --
 ALTER TABLE `admins`
-  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `admin_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `dental`
 --
 ALTER TABLE `dental`
-  MODIFY `dental_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `dental_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
 
 --
 -- AUTO_INCREMENT for table `dentalapp`
@@ -264,7 +290,13 @@ ALTER TABLE `dentalapp`
 -- AUTO_INCREMENT for table `healthrecord`
 --
 ALTER TABLE `healthrecord`
-  MODIFY `health_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=75;
+  MODIFY `health_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=76;
+
+--
+-- AUTO_INCREMENT for table `patientrecord`
+--
+ALTER TABLE `patientrecord`
+  MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `status`
@@ -276,7 +308,7 @@ ALTER TABLE `status`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;

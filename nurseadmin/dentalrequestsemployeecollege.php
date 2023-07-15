9161cfbe8af9ce1b9e2eb9df5d69a61413258d33
joannaatento <06802182@dwc-legazpi.edu>
2023-07-15 17:50:18 +0800

    medical for student college fixed

--- nurseadmin/dentalrequestsemployeecollege.php
+++ nurseadmin/dentalrequestsemployeecollege.php
@@ -249,7 +249,7 @@ if (mysqli_num_rows($result) > 0) {
                 <a href="function/functionemployeecollege.php?dental_id=<?php echo $dental_id; ?>">Mark as Read</a>
             <?php endif; ?>
 
-            <a href="function/deletedentalreqcollege.php?dental_id=<?php echo $dental_id; ?>" onclick="return confirm('Are you sure you want to delete this message?')">Deleted</a>
+            <a href="function/deletereqcollege.php?dental_id=<?php echo $dental_id; ?>" onclick="return confirm('Are you sure you want to delete this message?')">Deleted</a>
             
             <a href="viewdentalrequestemployeecollege.php?date_created=<?php echo $row['date_created']; ?>">
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-view-list" viewBox="0 0 16 16">

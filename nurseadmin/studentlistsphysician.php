98a1b03f21f85da06a1666aebaed0ab5bfbfbfca
joannaatento <06802182@dwc-legazpi.edu>
2023-07-15 19:33:00 +0800

    fixing sidebar in physician

--- nurseadmin/studentlistsphysician.php
+++ nurseadmin/studentlistsphysician.php
@@ -7,21 +7,6 @@
         echo '<script>window.location.replace("login.php");</script>';
         exit; // Exit the script to prevent further execution
     }
-    $admin_id = $_SESSION['admin_id'];
-    $sql_query = "SELECT * FROM admins WHERE admin_id ='$admin_id'";
-    $result = $conn->query($sql_query);
-    while($row = $result->fetch_array()){
-        $admin_id = $row['admin_id'];
-        $username = $row['username'];
-        require_once('../db.php');
-        if($_SESSION['role'] == 6){
-            // User type 1 specific code here
-        }
-        else{
-            header('location: ../login.php');
-            exit; // Exit the script to prevent further execution
-        }
-    }
 
 ?>
 
@@ -29,7 +14,7 @@
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
-    <title>Physician in GS, JHS, & SHS</title>
+    <title>CAPSTONE</title>
     
     <!-- Meta -->
     <meta charset="utf-8">
@@ -43,17 +28,13 @@
     <!-- FontAwesome JS-->
     <script defer src="assets/plugins/fontawesome/js/all.min.js"></script>
     
-    
     <!-- App CSS -->  
     <link id="theme-style" rel="stylesheet" href="assets/css/portal.css">
-	<link rel="stylesheet" href="assets/table.css">
-    
-    
-
+    <link rel="stylesheet" href="assets/table.css">
 </head> 
 
 <body class="app">   	
-    <header class="app-header fixed-top">	   	            
+<header class="app-header fixed-top">	   	            
         <div class="app-header-inner">  
 	        <div class="container-fluid py-2">
 		        <div class="app-header-content"> 
@@ -204,20 +185,80 @@
 				    <div class="app-card-header px-4 py-3">
 				        <div class="row g-3 align-items-center">
 					        <div class="col-12 col-lg-auto text-center text-lg-start">
-						        <h4 class="notification-title mb-1"></h4>
+						        <h4 class="notification-title mb-1">Students Health Profiles</h4>
 					        </div>
 							<!--//generate report-->
 				        </div><!--//row-->
 				    </div><!--//app-card-header-->
-				    <div class="app-card-body p-4">
-					   
-				    </div><!--//app-card-body-->
-				</div>			    
-		    </div>
-	    </div>
-    </div>  					
-    <!-- Javascript -->          
-    <script src="assets/plugins/popper.min.js"></script>
+                    <div class="app-card-header p-4 pb-2 border-0">
+  <div class="app-search-box col">
+    <form class="app-search-form" onsubmit="event.preventDefault(); searchRecords();">
+      <input type="text" placeholder="Search..." name="query" id="searchQuery" class="form-control search-input">
+      <button type="submit" class="btn search-btn btn-primary"><i class="fas fa-search"></i></button>
+    </form>
+  </div>
+</div>
+
+<div class="app-card-body p-4">
+  <div id="healthRecordTable">
+    <table>
+      <thead>
+        <tr>
+          <th>Student Name</th>
+          <th>Student ID Number</th>
+          <th>Level of Education</th>
+          <th>Age</th>
+          <th>Name of Person to Contact</th>
+          <th>Person to Contact Number</th>
+          <th>Relationship</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody id="healthRecordTableBody">
+        <?php
+        $sql = "SELECT * FROM healthrecord WHERE role = 'student'";
+        $result = mysqli_query($conn, $sql);
+
+        while($row = $result->fetch_assoc()){
+        ?>
+        <tr>
+          <td><?php echo $row['fullname']; ?></td>
+          <td><?php echo $row['idnumber']; ?></td>
+          <td><?php echo $row['leveleduc']; ?></td>
+          <td><?php echo $row['age']; ?></td>
+          <td><?php echo $row['nameperson']; ?></td>
+          <td><?php echo $row['personcp']; ?></td>
+          <td><?php echo $row['relationship']; ?></td>
+          <td>
+            <center><a href="viewstudentrecordphysicianside.php?idnumber=<?php echo $row['idnumber']; ?>">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-view-list" viewBox="0 0 16 16">
+                <path d="M3 4.5h10a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1H3zM1 2a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 2zm0 12a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 14z"/>
+              </svg>
+            </a></center>
+          </td>
+        </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
+</div>
+
+<script>
+function searchRecords() {
+  var searchQuery = document.getElementById("searchQuery").value;
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("healthRecordTableBody").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "search.php?query=" + searchQuery, true);
+  xhttp.send();
+}
+</script>
+
+ <!-- Javascript -->          
+ <script src="assets/plugins/popper.min.js"></script>
     <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>  
     
     <!-- Page Specific JS -->
@@ -232,7 +273,6 @@
 			}
 		}, 5000);
 	</script>
-
 
 </body>
 </html> 

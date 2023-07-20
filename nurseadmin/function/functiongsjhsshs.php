bc0db2940b66815de0b4bb3837f0dc4fbff1a3c0
joannaatento <06802182@dwc-legazpi.edu>
2023-07-20 21:53:16 +0800

    gsjhs have now own folder

--- nurseadmin/function/functiongsjhsshs.php
+++ nurseadmin/function/functiongsjhsshs.php
@@ -19,50 +19,8 @@ if (isset($_GET['dental_id'])) {
     exit();
 }
 
-if (isset($_GET['med_id'])) {
-    $med_id = $_GET['med_id'];
-
-    // Perform the necessary database update to mark the message as read
-    // Replace 'your_table_name' with the actual table name where you store the messages
-    $sql = "UPDATE medical SET is_read = 1 WHERE med_id = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $med_id);
-    $stmt->execute();
-
-    // Redirect the user back to the original page or any other appropriate page
-    header("Location: ../medicalrequestgsjhs.php");
-    exit();
-}
 
 
-
- 
-if(isset($_POST['submit_patientmngmt'])){ // pag get ng data
-    $admin_id = $_POST['admin_id']; 
-    $idnumber = $_POST['idnumber']; 
-    $fullname = $_POST['fullname']; 
-    $gradesection = $_POST['gradesection'];
-    $vitalsigns = $_POST['vitalsigns'];
-    $diagnosis = $_POST['diagnosis'];
-
-    date_default_timezone_set('Asia/Manila');
-    
-    $date_time = date('Y-m-d h:i A'); // Include time in the date with AM/PM format
-
-    $sql = "INSERT INTO patientrecord VALUES ('','$admin_id','$idnumber','$fullname','$gradesection','$vitalsigns','$diagnosis','$date_time')";
-    if(mysqli_query($conn, $sql)){
-        // echo "<script>window.history.go(-1);</script>";
-        header('location: ../patientmanagementrecordgsjhs.php');
-        echo $_SESSION['success'] ="
-            <div id='success-message' style='position:absolute; right:30px; background-color:#15a362; padding: 10px 10px; width:auto; border-radius: 10px;'>
-                <h2 style='
-                color: #fff;
-                font-size: 16px;
-                margin-left: 10px;'>Dental Appointment Added.</h2>
-            </div>
-        ";
-    }
-}
 
     if(isset($_POST['submit_schoolhealthassesform'])){ // pag get ng data
         $admin_id = $_POST['admin_id']; 

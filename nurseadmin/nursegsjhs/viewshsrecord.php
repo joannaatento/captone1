2733a9fbc45ee033091bebb53296fd7960edd8e5
joannaatento <06802182@dwc-legazpi.edu>
2023-07-25 15:19:43 +0800

    gsjhs nurse side the shslist is done

--- nurseadmin/nursegsjhs/viewshsrecord.php
+++ nurseadmin/nursegsjhs/viewshsrecord.php
@@ -2,34 +2,19 @@
     session_start();
     include '../../db.php';
 
-    if (!isset($_SESSION['user_id'])){
+    if (!isset($_SESSION['admin_id'])){
         echo '<script>window.alert("PLEASE LOGIN FIRST!!")</script>';
         echo '<script>window.location.replace("login.php");</script>';
         exit; // Exit the script to prevent further execution
     }
 
-    $user_id = $_SESSION['user_id'];
-    $sql_query = "SELECT * FROM users WHERE user_id ='$user_id'";
-    $result = $conn->query($sql_query);
-    while($row = $result->fetch_array()){
-        $user_id = $row['user_id'];
-        $fullname = $row['fullname'];
-        require_once('../../db.php');
-        if($_SESSION['leveleduc'] == 2){
-            // User type 1 specific code here
-        }
-        else{
-            header('location: ../login.php');
-            exit; // Exit the script to prevent further execution
-        }
-    }
+  
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
-    <title>Health Profile Form</title>
+    <title>View Health Profile Record</title>
     
     <!-- Meta -->
     <meta charset="utf-8">
@@ -45,12 +30,67 @@
     
     <!-- App CSS -->  
     <link id="theme-style" rel="stylesheet" href="assets/css/portal.css">
-	<link rel="stylesheet" href="assets/formstyles.css">
+	<link rel="stylesheet" href="assets/formstyle.css">
 
 </head> 
 
-<body class="app">   	
-<header class="app-header fixed-top">	   	            
+<body class="app">   
+    
+<?php  	
+$idnumber = $_GET['idnumber'];
+
+// Retrieve the health record for the given ID number
+$sql = "SELECT * FROM healthrecordformshs WHERE idnumber = '$idnumber'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  $row = $result->fetch_assoc(); 
+  $image = $row['image'];
+  $gradelevel = $row['gradelevel'];
+  $fullname= $row['fullname'];
+  $idnumber = $row['role'];
+  $pcontact = $row['pcontact'];
+  $age = $row['idnumber'];
+  $gender = $row['gender'];
+  $address = $row ['address'];
+  $paddress = $row ['paddress'];
+  $father = $row['father'];
+  $cfather = $row['cfather'];
+  $mother = $row['mother'];
+  $cmother = $row['cmother'];
+  $polio = $row['polio'];
+  $tetanus = $row['tetanus'];
+  $chickenpox = $row['chickenpox'];
+  $measles = $row['measles'];
+  $mumps = $row['mumps'];
+  $tb = $row['tb'];
+  $asthma = $row['asthma'];
+  $hepatitis = $row['hepatitis'];
+  $faintingspells = $row['faintingspells'];
+  $seizure = $row['seizure'];
+  $bleeding = ['bleeding'];
+  $eyedis = $row['eyedis'];
+  $heartailment = $row['heartailment'];
+  $otherillness = $row['otherillness'];
+  $yesfood =$row ['yesfood'];
+  $nofood = $row['nofood'];
+  $food =$row['food'];
+  $yesmed = $row['yesmed'];
+  $nomed = $row['nomed'];
+  $med = $row['med'];
+  $allow = $row['allow'];
+  $notallow = $row['notallow'];
+  $medpresent =$row['medpresent'];
+  $notified =$row['notified'];
+  $contact = $row['contact'];
+  $relationship =$row['relationship'];
+
+
+    }
+ else {
+ } 
+?>
+    <header class="app-header fixed-top">	   	            
         <div class="app-header-inner">  
 	        <div class="container-fluid py-2">
 		        <div class="app-header-content"> 
@@ -61,9 +101,15 @@
 					    </a>
 				    </div><!--//col-->
 		            <div class="app-utilities col-auto">		            
-			            <div class="app-utility-item app-user-dropdown dropdown">
+					<div class="app-utility-item app-user-dropdown dropdown">
+				            <img src="assets/images/user.png">
+				             <div class="app-utility-item app-user-dropdown dropdown">
 
-				            <a class="dropdown-toggle" id="user-dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"><img src="assets/images/user.png"><?= $fullname;?></a>
+                   <?php  if (isset($_SESSION['username'])) : ?>
+                                    <p><?php echo $_SESSION['username']; ?></p>
+                                    <?php endif ?></a>
+                   </div>
+				   <a class="dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"></a>
 				            <ul class="dropdown-menu" aria-labelledby="user-dropdown-toggle">
 								<li><a class="dropdown-item" href="function/logout.php">Log Out</a></li>
 							</ul>
@@ -82,81 +128,97 @@
 		        </div>
 			    <nav id="app-nav-main" class="app-nav app-nav-main flex-grow-1">
 				<ul class="app-menu list-unstyled accordion" id="menu-accordion">
-                <nav id="app-nav-main" class="app-nav app-nav-main flex-grow-1">
-				<ul class="app-menu list-unstyled accordion" id="menu-accordion">
-                <li class="nav-item has-submenu">
-
-    <a class="nav-link submenu-toggle active" href="healthrecordformshs.php" data-bs-target="#submenu-4" aria-controls="submenu-4">
-        <span class="nav-icon">
-            <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
-           <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-files" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <li class="nav-item has-submenu">
+        <a class="nav-link submenu-toggle active" href="#" data-bs-toggle="collapse" data-bs-target="#submenu-1" aria-expanded="false" aria-controls="submenu-1">
+            <span class="nav-icon">
+                <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-files" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                     <path fill-rule="evenodd" d="M4 2h7a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H4z"/>
                     <path d="M6 0h7a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2v-1a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1H4a2 2 0 0 1 2-2z"/>
                 </svg>
+            </span>
+            <span class="nav-link-text">Health Profiles</span>
+            <span class="submenu-arrow">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                </svg>
+            </span>
+        </a>
+        <div id="submenu-1" class="collapse submenu submenu-1" data-bs-parent="#menu-accordion">
+            <ul class="submenu-list list-unstyled">
+                <li class="submenu-item"><a class="submenu-link" href="gsjhslists.php">Grade School and Junior High School Building</a></li>
+                <li class="submenu-item"><a class="submenu-link" href="shslist.php">Senior High School Building</a></li>
+                <li class="submenu-item"><a class="submenu-link" href="collegelists.php">College Building</a></li>
+            </ul>
+        </div>
+    </li>
+
+<li class="nav-item has-submenu">
+    <a class="nav-link submenu-toggle active" href="#" data-bs-toggle="collapse" data-bs-target="#submenu-5" aria-expanded="false" aria-controls="submenu-5">
+        <span class="nav-icon">
+            <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+            </svg>
         </span>
-        <span class="nav-link-text">Health Profile</span>
+        <span class="nav-link-text">Medical Requests</span>
+        <span class="submenu-arrow">
+            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            </svg>
+        </span>
+    </a>
+    <div id="submenu-5" class="collapse submenu submenu-3" data-bs-parent="#menu-accordion">
+        <ul class="submenu-list list-unstyled">
+            <li class="submenu-item"><a class="submenu-link" href="medicalrequestgsjhs.php">Grade School and JHS</a></li>
+            <li class="submenu-item"><a class="submenu-link" href="medicalrequestsemployeegsjhs.php">Employee</a></li>
+        </ul>
+    </div>
+</li>
+
+    
+<li class="nav-item has-submenu">
+    <a class="nav-link submenu-toggle active" href="medicalgsjhs.php" data-bs-target="#submenu-4" aria-controls="submenu-4">
+        <span class="nav-icon">
+            <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-journal-check" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M10.854 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+            <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
+            <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
+            </svg>
+        </span>
+        <span class="nav-link-text">Medical Approved Appointments</span>
     </a>
 </li>
 
-
-
-	<li class="nav-item has-submenu">
-								<a class="nav-link submenu-toggle active" href="#" data-bs-toggle="collapse" data-bs-target="#submenu-2" aria-expanded="false" aria-controls="submenu-2">
-									<span class="nav-icon">
-										<!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
-										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope-plus" viewBox="0 0 16 16">
-											<path d="M2 2a2 2 0 0 0-2 2v8.01A2 2 0 0 0 2 14h5.5a.5.5 0 0 0 0-1H2a1 1 0 0 1-.966-.741l5.64-3.471L8 9.583l7-4.2V8.5a.5.5 0 0 0 1 0V4a2 2 0 0 0-2-2H2Zm3.708 6.208L1 11.105V5.383l4.708 2.825ZM1 4.217V4a1 1 0 0 1 1-1h12a1 1 0 0 1 1 1v.217l-7 4.2-7-4.2Z"/>
-											<path d="M16 12.5a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Zm-3.5-2a.5.5 0 0 0-.5.5v1h-1a.5.5 0 0 0 0 1h1v1a.5.5 0 0 0 1 0v-1h1a.5.5 0 0 0 0-1h-1v-1a.5.5 0 0 0-.5-.5Z"/>
-											</svg>
-									</span>
-									<span class="nav-link-text">Request Scheduling Appointment</span>
-									<span class="submenu-arrow">
-										<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-											<path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-										</svg>
-									</span>
-								</a>
-								<div id="submenu-2" class="collapse submenu submenu-2" data-bs-parent="#menu-accordion">
-									<ul class="submenu-list list-unstyled">
-										<li class="submenu-item"><a class="submenu-link" href="adddentalmessageshs.php">Request Dental Scheduling</a></li>
-										<li class="submenu-item"><a class="submenu-link" href="addmedicalmessageshs.php">Request Medical Scheduling</a></li>
-										<li class="submenu-item"><a class="submenu-link" href="addphysicianmessageshs.php">Request Physician Scheduling</a></li>
-									</ul>
-								</div>
-							</li>
-
-
-							<li class="nav-item has-submenu">
-								<a class="nav-link submenu-toggle active" href="#" data-bs-toggle="collapse" data-bs-target="#submenu-3" aria-expanded="false" aria-controls="submenu-3">
-									<span class="nav-icon">
-										<!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
-										<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-stickies" viewBox="0 0 16 16">
-										<path d="M1.5 0A1.5 1.5 0 0 0 0 1.5V13a1 1 0 0 0 1 1V1.5a.5.5 0 0 1 .5-.5H14a1 1 0 0 0-1-1H1.5z"/>
-										<path d="M3.5 2A1.5 1.5 0 0 0 2 3.5v11A1.5 1.5 0 0 0 3.5 16h6.086a1.5 1.5 0 0 0 1.06-.44l4.915-4.914A1.5 1.5 0 0 0 16 9.586V3.5A1.5 1.5 0 0 0 14.5 2h-11zM3 3.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 .5.5V9h-4.5A1.5 1.5 0 0 0 9 10.5V15H3.5a.5.5 0 0 1-.5-.5v-11zm7 11.293V10.5a.5.5 0 0 1 .5-.5h4.293L10 14.793z"/>
-										</svg>
-									</span>
-									<span class="nav-link-text">Clinic Records</span>
-									<span class="submenu-arrow">
-										<svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-											<path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
-										</svg>
-									</span>
-								</a>
-								<div id="submenu-3" class="collapse submenu submenu-3" data-bs-parent="#menu-accordion">
-									<ul class="submenu-list list-unstyled">
-									<li class="submenu-item"> <a class="submenu-link" href="viewhealthrecordprofileshs.php">Health Profile Record</a>
-									<li class="submenu-item"> <a class="submenu-link" href="viewdentalappshs.php">Dental Record</a>
-                                    <li class="submenu-item"> <a class="submenu-link" href="viewmedicalappshs.php">Medical Record</a>
-                                    <li class="submenu-item"> <a class="submenu-link" href="viewphysicianappshs.php">Physician Record</a>
-									<li class="submenu-item"> <a class="submenu-link" href="viewdiagnosisappshs.php">Diagnosis/Chief Complaints, Management & Treatment Record</a>
-									 <li class="submenu-item"> <a class="submenu-link" href="viewconsultationformshs.php">Consultation</a>
-									<li class="submenu-item"> <a class="submenu-link" href="viewschoolassesshs.php">School Health Assessment</a>
+    <li class="nav-item has-submenu">
+    <a class="nav-link submenu-toggle active" href="patientmanagementrecordgsjhs.php" data-bs-target="#submenu-4" aria-controls="submenu-4">
+        <span class="nav-icon">
+            <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-journal-text" viewBox="0 0 16 16">
+            <path d="M5 10.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 0 1h-2a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5zm0-2a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5z"/>
+            <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
+            <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
+            </svg>
+        </span>
+        <span class="nav-link-text">Patient's Management Record</span>
+    </a>
 </li>
-									</ul>
-								</div>
-							</li>
-				    </ul>
-			    </nav>
+
+<li class="nav-item has-submenu">
+    <a class="nav-link submenu-toggle active" href="schoolhealthassessmentformgsjhs.php" data-bs-target="#submenu-4" aria-controls="submenu-4">
+        <span class="nav-icon">
+            <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-medical" viewBox="0 0 16 16">
+            <path d="M8.5 4.5a.5.5 0 0 0-1 0v.634l-.549-.317a.5.5 0 1 0-.5.866L7 6l-.549.317a.5.5 0 1 0 .5.866l.549-.317V7.5a.5.5 0 1 0 1 0v-.634l.549.317a.5.5 0 1 0 .5-.866L9 6l.549-.317a.5.5 0 1 0-.5-.866l-.549.317V4.5zM5.5 9a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5zm0 2a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z"/>
+            <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>
+            </svg>
+        </span>
+        <span class="nav-link-text">School Health Assessment Form</span>
+    </a>
+</li>
+</ul>
+	</nav>
 	        </div>
 	    </div>
     </header>
@@ -189,18 +251,10 @@
 				        </div><!--//row-->
 				    </div><!--//app-card-header-->
 				    <div class="app-card-body p-4">
-                    <?php
-							$sql = "SELECT * FROM healthrecordformshs WHERE user_id = '$user_id'";
-							$result = $conn->query($sql);
-    						while($row = $result->fetch_array()){
-						?>
+                  
 					<p class="title_">Personal Information</p>
-					
-					<form class="form-horizontal mt-4" action="function/funct.php" method="POST" enctype="multipart/form-data">
-    <div class="align_form">
-
-	
-    <div class="align_form">
+                    <div class="app-card-body p-4">
+							<div class="align_form">
 								<div class="input_form">
 								<div class="input_wrap">
 							<label></label>
@@ -216,17 +270,16 @@
         <input id="gradelevel" name="gradelevel" type="text" value="<?=$row['gradelevel'];?>" readonly >
     </div>
     <div class="input_wrap">
-        <label for="fullname">Full Name</label>
-        <input id="fullname" name="fullname" type="text" value="<?= $fullname; ?>" >
-    </div>
-            
-    <div class="input_wrap">
                 <label for="fullname">Role</label>
                 <select class="form-select" name="role">
                 <option disabled selected><?= $row['role'];?></option>
                 </select>
         </div>
-            </div>
+<div class="input_wrap">
+        <label for="fullname">Full Name</label>
+        <input id="fullname" name="fullname" type="text" value="<?= $fullname; ?>" >
+    </div>
+                    </div>
                     </div>
                             </div>
                     <br><br>
@@ -417,8 +470,8 @@
 
    <div class="input_form"> 
             <div class="input_wrap">
-                <label for="fullname" >Person to be notified in case of emergency:</label>
-                <input name="notified" id ="languages" type="text" value="<?=$row['notified'];?>" readonly>
+                <label for="fullname">Person to be notified in case of emergency:</label>
+                <input name="notified" id ="languagessss" type="text" value="<?=$row['notified'];?>" readonly>
             </div>
             <div class="input_wrap">
                 <label for="fullname">Contact Number</label>
@@ -430,9 +483,6 @@
             </div>
    </div>
 
-   <?php
-							}
-						?>
 
 </form>
 

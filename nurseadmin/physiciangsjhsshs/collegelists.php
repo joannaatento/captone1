4fba901522617343897770bd3dc777ef5ef39ecd
joannaatento <06802182@dwc-legazpi.edu>
2023-07-28 15:58:56 +0800

    physiciangsjhsshs collegelists done

---
+++ nurseadmin/physiciangsjhsshs/collegelists.php
@@ -0,0 +1,249 @@
+<?php
+    session_start();
+    include '../../db.php';
+
+    if (!isset($_SESSION['admin_id'])){
+        echo '<script>window.alert("PLEASE LOGIN FIRST!!")</script>';
+        echo '<script>window.location.replace("../login.php");</script>';
+        exit; // Exit the script to prevent further execution
+    }
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en"> 
+<head>
+    <title>College Building</title>
+    
+    <!-- Meta -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="description" content="Portal - Bootstrap 5 Admin Dashboard Template For Developers">
+    <meta name="author" content="Xiaoying Riley at 3rd Wave Media">    
+    <link rel="shortcut icon" href="assets/images/dwcl.png"> 
+    
+    <!-- FontAwesome JS-->
+    <script defer src="assets/plugins/fontawesome/js/all.min.js"></script>
+    
+    <!-- App CSS -->  
+    <link id="theme-style" rel="stylesheet" href="assets/css/portal.css">
+    <link rel="stylesheet" href="assets/tableform.css">
+
+</head> 
+
+<body class="app">   	
+<header class="app-header fixed-top">	   	            
+        <div class="app-header-inner">  
+	        <div class="container-fluid py-2">
+		        <div class="app-header-content"> 
+		            <div class="row justify-content-between align-items-center">
+				    <div class="col-auto">
+					    <a id="sidepanel-toggler" class="sidepanel-toggler d-inline-block d-xl-none" href="#">
+						    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30" role="img"><title>Menu</title><path stroke="currentColor" stroke-linecap="round" stroke-miterlimit="10" stroke-width="2" d="M4 7h22M4 15h22M4 23h22"></path></svg>
+					    </a>
+				    </div><!--//col-->
+		            <div class="app-utilities col-auto">		            
+					<div class="app-utility-item app-user-dropdown dropdown">
+				            <img src="assets/images/user.png">
+				             <div class="app-utility-item app-user-dropdown dropdown">
+
+                   <?php  if (isset($_SESSION['username'])) : ?>
+                                    <p><?php echo $_SESSION['username']; ?></p>
+                                    <?php endif ?></a>
+                   </div>
+				   <a class="dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false"></a>
+				            <ul class="dropdown-menu" aria-labelledby="user-dropdown-toggle">
+								<li><a class="dropdown-item" href="function/logout.php">Log Out</a></li>
+							</ul>
+			            </div>
+		            </div>
+		        </div>
+	            </div>
+	        </div>
+        </div>
+        <div id="app-sidepanel" class="app-sidepanel sidepanel-hidden"> 
+			<div id="sidepanel-drop" class="sidepanel-drop"></div>
+				<div class="sidepanel-inner d-flex flex-column">
+		        <a href="#" id="sidepanel-close" class="sidepanel-close d-xl-none">&times;</a>
+		        <div class="app_logo">
+					<img style="width: 150px; display:flex; margin-left: 50px; margin-top: 10px;" src="assets/images/dwcl.png" alt="logo">
+		        </div>
+			    <nav id="app-nav-main" class="app-nav app-nav-main flex-grow-1">
+				<ul class="app-menu list-unstyled accordion" id="menu-accordion">
+    <li class="nav-item has-submenu">
+        <a class="nav-link submenu-toggle active" href="#" data-bs-toggle="collapse" data-bs-target="#submenu-1" aria-expanded="false" aria-controls="submenu-1">
+            <span class="nav-icon">
+                <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-files" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M4 2h7a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h7a1 1 0 0 0 1-1V4a1 1 0 0 0-1-1H4z"/>
+                    <path d="M6 0h7a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2v-1a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H6a1 1 0 0 0-1 1H4a2 2 0 0 1 2-2z"/>
+                </svg>
+            </span>
+            <span class="nav-link-text">Health Profiles</span>
+            <span class="submenu-arrow">
+                <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+                </svg>
+            </span>
+        </a>
+        <div id="submenu-1" class="collapse submenu submenu-1" data-bs-parent="#menu-accordion">
+            <ul class="submenu-list list-unstyled">
+            <li class="submenu-item"><a class="submenu-link" href="gsjhslists.php">Grade School and Junior High School Building</a></li>
+                <li class="submenu-item"><a class="submenu-link" href="shslist.php">Senior High School Building</a></li>
+                <li class="submenu-item"><a class="submenu-link" href="collegelists.php">College Building</a></li>
+            </ul>
+        </div>
+    </li>
+
+    <li class="nav-item has-submenu">
+    <a class="nav-link submenu-toggle active" href="#" data-bs-toggle="collapse" data-bs-target="#submenu-3" aria-expanded="false" aria-controls="submenu-3">
+        <span class="nav-icon">
+            <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+                  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V4Zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1H2Zm13 2.383-4.708 2.825L15 11.105V5.383Zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741ZM1 11.105l4.708-2.897L1 5.383v5.722Z"/>
+            </svg>
+        </span>
+        <span class="nav-link-text">Physician Consultation Request</span>
+        <span class="submenu-arrow">
+            <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-chevron-down" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+            </svg>
+        </span>
+    </a>
+    <div id="submenu-3" class="collapse submenu submenu-3" data-bs-parent="#menu-accordion">
+        <ul class="submenu-list list-unstyled">
+            <li class="submenu-item"><a class="submenu-link" href="physicianstudentgsjhsshs.php">Student</a></li>
+            <li class="submenu-item"><a class="submenu-link" href="physicianemployeegsjhsshs.php">Employee</a></li>
+        </ul>
+    </div>
+</li>
+
+    
+<li class="nav-item has-submenu">
+    <a class="nav-link submenu-toggle active" href="physicianapproved.php" data-bs-target="#submenu-4" aria-controls="submenu-4">
+        <span class="nav-icon">
+            <!--//Bootstrap Icons: https://icons.getbootstrap.com/ -->
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-journal-check" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M10.854 6.146a.5.5 0 0 1 0 .708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 8.793l2.646-2.647a.5.5 0 0 1 .708 0z"/>
+            <path d="M3 0h10a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-1h1v1a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H3a1 1 0 0 0-1 1v1H1V2a2 2 0 0 1 2-2z"/>
+            <path d="M1 5v-.5a.5.5 0 0 1 1 0V5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0V8h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1zm0 3v-.5a.5.5 0 0 1 1 0v.5h.5a.5.5 0 0 1 0 1h-2a.5.5 0 0 1 0-1H1z"/>
+            </svg>
+        </span>
+        <span class="nav-link-text">Physician Consultation Appointments</span>
+    </a>
+</li>
+</ul>
+  </nav>
+	        </div>
+	    </div>
+    </header>
+    
+    <div class="app-wrapper">
+	    
+	    <div class="app-content pt-3 p-md-3 p-lg-4">
+		    <div class="container-xl">
+			    <div class="position-relative mb-3">
+				    <div class="row g-3 justify-content-between">
+					    <div class="col-auto">
+					        <h1 class="app-page-title mb-0"></h1>
+					    </div>
+						
+				    </div>
+			    </div>
+			    
+                <div class="app-card app-card-notification shadow-sm mb-4">
+				    <div class="app-card-header px-4 py-3">
+				        <div class="row g-3 align-items-center">
+					        <div class="col-12 col-lg-auto text-center text-lg-start">
+						        <h4 class="notification-title mb-1">College Health Profiles</h4>
+					        </div>
+							<!--//generate report-->
+				        </div><!--//row-->
+				    </div><!--//app-card-header-->
+                    <div class="app-card-header p-4 pb-2 border-0">
+  <div class="app-search-box col">
+    <form class="app-search-form" onsubmit="event.preventDefault(); searchRecords();">
+      <input type="text" placeholder="Search..." name="query" id="searchQuery" class="form-control search-input">
+      <button type="submit" class="btn search-btn btn-primary"><i class="fas fa-search"></i></button>
+    </form>
+  </div>
+</div>
+
+<div class="app-card-body p-4">
+  <div id="healthRecordTable">
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>ID Number</th>
+          <th>Course & Year</th>
+          <th>Person to Contact</th>
+          <th>Contact Person Number</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody id="healthRecordTableBody">
+        <?php
+        $sql = "SELECT * FROM healthrecordformcollege";
+        $result = mysqli_query($conn, $sql);
+
+        while($row = $result->fetch_assoc()){
+        ?>
+        <tr>
+          <td><?php echo $row['fullname']; ?></td>
+          <td><?php echo $row['idnumber']; ?></td>
+          <td><?php echo $row['courseyear']; ?></td>
+          <td><?php echo $row['contactemer']; ?></td>
+          <td><?php echo $row['contactno']; ?></td>
+          
+          <td>
+            <center><a href="viewcollegerecord.php?idnumber=<?php echo $row['idnumber']; ?>">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-view-list" viewBox="0 0 16 16">
+                <path d="M3 4.5h10a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1H3zM1 2a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 2zm0 12a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 14z"/>
+              </svg>
+            </a></center>
+          </td>
+        </tr>
+        <?php } ?>
+      </tbody>
+    </table>
+  </div>
+</div>
+
+<script>
+function searchRecords() {
+  var searchQuery = document.getElementById("searchQuery").value;
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("healthRecordTableBody").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "function/searchqueryforcollege.php?query=" + searchQuery, true);
+  xhttp.send();
+}
+</script>
+
+ <!-- Javascript -->          
+ <script src="assets/plugins/popper.min.js"></script>
+    <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>  
+    
+    <!-- Page Specific JS -->
+    <script src="assets/js/app.js"></script> 
+	
+	<script>
+		// Timer to remove success message after 5 seconds (5000 milliseconds)
+		setTimeout(function(){
+			var successMessage = document.getElementById('success-message');
+			if(successMessage){
+				successMessage.remove();
+			}
+		}, 5000);
+	</script>
+
+</body>
+</html> 
+

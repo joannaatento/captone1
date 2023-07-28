a64d6c247482f4f2e5291c71c481d346600cdcf8
joannaatento <06802182@dwc-legazpi.edu>
2023-07-28 21:58:29 +0800

    weight monitoring sheet can now viewed by usershs

--- user/usershs/viewweightmonitoringshs.php
+++ user/usershs/viewweightmonitoringshs.php
@@ -14,12 +14,13 @@
     while($row = $result->fetch_array()){
         $user_id = $row['user_id'];
         $fullname = $row['fullname'];
+        $idnumber = $row['idnumber'];
         require_once('../../db.php');
         if($_SESSION['leveleduc'] == 2){
             // User type 1 specific code here
         }
         else{
-            header('location: ../login.php');
+            header('location: ../../login.php');
             exit; // Exit the script to prevent further execution
         }
     }
@@ -29,7 +30,7 @@
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
-    <title>User Dashboard</title>
+    <title>View Weight Monitoring Record</title>
     
     <!-- Meta -->
     <meta charset="utf-8">
@@ -50,6 +51,25 @@
 </head> 
 
 <body class="app">   	
+<?php
+
+// Retrieve the health record for the given ID number
+$sql = "SELECT * FROM weightmonitor WHERE idnumber= '$idnumber'";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+  $row = $result->fetch_assoc(); 
+  $idnumber = $row['idnumber'];
+  $fullname = $row['fullname'];
+  $age = $row['age'];
+  $gradesection = $row['gradesection'];
+  $date_time = $row['date_time'];
+  $weight = $row['weight'];
+  $remarks = $row['remarks'];
+    }
+ else {
+ } 
+?>
 <header class="app-header fixed-top">	   	            
         <div class="app-header-inner">  
 	        <div class="container-fluid py-2">
@@ -151,7 +171,6 @@
 									<li class="submenu-item"> <a class="submenu-link" href="viewdiagnosisappshs.php">Diagnosis/Chief Complaints, Management & Treatment Record</a>
 									 <li class="submenu-item"> <a class="submenu-link" href="viewconsultationformshs.php">Consultation Form Record</a>
 									<li class="submenu-item"> <a class="submenu-link" href="viewschoolassesshs.php">School Health Assessment</a>
-									<li class="submenu-item"> <a class="submenu-link" href="viewweightmonitoringshs.php">Weight Monitoring Record</a>
 </li>
 									</ul>
 								</div>
@@ -161,32 +180,94 @@
 	        </div>
 	    </div>
     </header>
-    
     <div class="app-wrapper">
-	    
-	    <div class="app-content pt-3 p-md-3 p-lg-4">
-		    <div class="container-xl">
-			    <div class="position-relative mb-3">
-				    <div class="row g-3 justify-content-between">
-					   
-					       
-						
-				    </div>
-			    </div>
-			    
-                <div class="app-card app-card-notification shadow-sm mb-4">
-				    <div class="app-card-header px-4 py-3">
-				        <div class="row g-3 align-items-center">
-					       
-				        </div><!--//row-->
-				    </div><!--//app-card-header-->
-				    <div class="app-card-body p-4">
-				
-				    </div><!--//app-card-body-->
-				</div>			    
-		    </div>
-	    </div>
-    </div>  					
+    <div class="app-content pt-3 p-md-3 p-lg-4">
+        <div class="container-xl">
+            <div class="position-relative mb-3">
+                <div class="row g-3 justify-content-between">
+                </div>
+            </div>
+            
+            <div class="app-card app-card-notification shadow-sm mb-4">
+                <div class="app-card-header px-4 py-3">
+                    <div class="row g-3 align-items-center">
+                    </div><!--//row-->
+                </div><!--//app-card-header-->
+                <div class="app-card-body p-4">
+                    <?php
+                    $sql = "SELECT * FROM weightmonitor WHERE idnumber = '$idnumber'";
+                    $result = $conn->query($sql);
+                    while($row = $result->fetch_array()) {
+                    ?>
+                    <br>
+        
+<div class="row">
+                  <div class="col-sm-3">
+                      <div class="form-group">
+                          <label for="idnumber" class="col-sm-7 control-label" style="font-size: 16px">ID Number</label>
+                          <div class="col-sm-11">
+                              <input type="text" class="form-control" id="idnumber" name="idnumber" value="<?php echo $row['idnumber']; ?>" readonly>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-sm-3">
+                      <div class="form-group">
+                          <label for="fullname" class="col-sm-4 control-label" style="font-size: 16px">Name</label>
+                          <div class="col-sm-11">
+                              <input type="text" class="form-control" id="fullname" name="fullname" value="<?php echo $row['fullname']; ?>" readonly>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-sm-3">
+                      <div class="form-group">
+                          <label for="age" class="col-sm-4 control-label" style="font-size: 16px">Age</label>
+                          <div class="col-sm-11">
+                              <input type="age" class="form-control" id="age" name="age" value="<?php echo $row['age']; ?>" readonly>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-sm-3">
+                      <div class="form-group">
+                          <label for="gradesection" class="col-sm-10 control-label" style="font-size: 16px">Employee/Grade & Section</label>
+                          <div class="col-sm-11">
+                              <input type="text" class="form-control" id="gradesection" name="gradesection" value="<?php echo $row['gradesection']; ?>" readonly>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+            
+              <div class="row">
+                  
+                  <div class="col-sm-3">
+                    <br>
+                      <div class="form-group">
+                          <label for="weight" class="col-sm-5 control-label" style="font-size: 16px">Weight (kg)</label>
+                          <div class="col-sm-11">
+                              <input type="text" class="form-control" id="weight" name="weight" value="<?php echo $row['weight']; ?>" readonly>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-sm-8">
+                    <br>
+                      <div class="form-group">
+                          <label for="remarks" class="col-sm-4 control-label" style="font-size: 16px">Remarks</label>
+                          <div class="col-sm-15">
+                              <input type="text" class="form-control" id="remarks" name="remarks" value="<?php echo $row['remarks']; ?>" readonly>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="form-group">
+                            <span><b>Date & Time:</b> <?php echo $row['date_time']; ?></span>
+                        </div>
+
+                    <?php } ?>
+                </div><!--//app-card-body-->
+            </div>
+        </div>
+    </div>
+</div>
+
     <!-- Javascript -->          
     <script src="assets/plugins/popper.min.js"></script>
     <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>  

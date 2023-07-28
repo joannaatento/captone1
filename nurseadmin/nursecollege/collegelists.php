aae86153436501fe047b3d46eb3f123f5490e7d2
joannaatento <06802182@dwc-legazpi.edu>
2023-07-28 14:18:00 +0800

    nursecollege collegelists done

--- nurseadmin/nursecollege/collegelists.php
+++ nurseadmin/nursecollege/collegelists.php
@@ -14,7 +14,7 @@
 <!DOCTYPE html>
 <html lang="en"> 
 <head>
-    <title>Grade School and Junior High School Building</title>
+    <title>College Building</title>
     
     <!-- Meta -->
     <meta charset="utf-8">
@@ -34,7 +34,7 @@
 </head> 
 
 <body class="app">   	
-    <header class="app-header fixed-top">	   	            
+<header class="app-header fixed-top">	   	            
         <div class="app-header-inner">  
 	        <div class="container-fluid py-2">
 		        <div class="app-header-content"> 
@@ -167,6 +167,7 @@
 	        </div>
 	    </div>
     </header>
+    
     <div class="app-wrapper">
 	    
 	    <div class="app-content pt-3 p-md-3 p-lg-4">
@@ -184,7 +185,7 @@
 				    <div class="app-card-header px-4 py-3">
 				        <div class="row g-3 align-items-center">
 					        <div class="col-12 col-lg-auto text-center text-lg-start">
-						        <h4 class="notification-title mb-1">Grade School and Junior High School Health Profiles</h4>
+						        <h4 class="notification-title mb-1">College Health Profiles</h4>
 					        </div>
 							<!--//generate report-->
 				        </div><!--//row-->
@@ -205,7 +206,7 @@
         <tr>
           <th>Name</th>
           <th>ID Number</th>
-          <th>Age</th>
+          <th>Course & Year</th>
           <th>Person to Contact</th>
           <th>Contact Person Number</th>
           <th>Action</th>
@@ -213,7 +214,7 @@
       </thead>
       <tbody id="healthRecordTableBody">
         <?php
-        $sql = "SELECT * FROM healthrecordformgsjhs";
+        $sql = "SELECT * FROM healthrecordformcollege";
         $result = mysqli_query($conn, $sql);
 
         while($row = $result->fetch_assoc()){
@@ -221,12 +222,12 @@
         <tr>
           <td><?php echo $row['fullname']; ?></td>
           <td><?php echo $row['idnumber']; ?></td>
-          <td><?php echo $row['age']; ?></td>
-          <td><?php echo $row['guardianname']; ?></td>
-          <td><?php echo $row['cguardian']; ?></td>
+          <td><?php echo $row['courseyear']; ?></td>
+          <td><?php echo $row['contactemer']; ?></td>
+          <td><?php echo $row['contactno']; ?></td>
           
           <td>
-            <center><a href="viewgsjhsrecord.php?idnumber=<?php echo $row['idnumber']; ?>">
+            <center><a href="viewcollegerecord.php?idnumber=<?php echo $row['idnumber']; ?>">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-view-list" viewBox="0 0 16 16">
                 <path d="M3 4.5h10a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2zm0 1a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h10a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1H3zM1 2a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 2zm0 12a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13A.5.5 0 0 1 1 14z"/>
               </svg>
@@ -248,7 +249,7 @@ function searchRecords() {
       document.getElementById("healthRecordTableBody").innerHTML = this.responseText;
     }
   };
-  xhttp.open("GET", "function/searchqueryforgsjhs.php?query=" + searchQuery, true);
+  xhttp.open("GET", "function/searchqueryforcollege.php?query=" + searchQuery, true);
   xhttp.send();
 }
 </script>

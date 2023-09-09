2e6a190d2e82026cb1fefb436fb2dcb2ad2682c7
joannaatento <06802182@dwc-legazpi.edu>
2023-09-09 10:21:41 +0800

    totalmedicines nursegsjhs can now print

--- nurseadmin/nursegsjhs/totalmedicines.php
+++ nurseadmin/nursegsjhs/totalmedicines.php
@@ -237,57 +237,81 @@
 				        </div><!--//row-->
 				    </div><!--//app-card-header-->
                     <?php
-
-
 $selected_year = isset($_POST['selected_year']) ? $_POST['selected_year'] : '2023';
+$report_type = isset($_POST['report_type']) ? $_POST['report_type'] : 'week';
+
+// Your database connection code here
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (isset($_POST['report_type']) && isset($_POST['selected_year'])) {
         $report_type = $_POST['report_type'];
         $selected_year = $_POST['selected_year'];
+    }
+}
 
-        switch ($report_type) {
-            case 'week':
-                $sql = "SELECT CONCAT(YEAR(date_created), '-', WEEK(date_created)) AS label,
-                        medicine_name,
-                        COUNT(medicine_name) AS total_medicine,
-                        SUM(quantity) AS total_quantity
-                        FROM medicine
-                        WHERE role = '1' AND YEAR(date_created) = $selected_year
-                        GROUP BY label, medicine_name";
-                $report_label = 'Weekly';
-                break;
+switch ($report_type) {
+    case 'week':
+        $sql = "SELECT CONCAT(YEAR(date_created), '-', WEEK(date_created)) AS label,
+                medicine_name,
+                COUNT(medicine_name) AS total_medicine,
+                SUM(quantity) AS total_quantity
+                FROM medicine
+                WHERE role = '1' AND YEAR(date_created) = ?
+                GROUP BY label, medicine_name";
+        $report_label = 'Weekly';
+        break;
 
-            case 'month':
-                $sql = "SELECT CONCAT(YEAR(date_created), '-', MONTHNAME(date_created)) AS label,
-                        medicine_name,
-                        COUNT(medicine_name) AS total_medicine,
-                        SUM(quantity) AS total_quantity
-                        FROM medicine
-                        WHERE role = '1' AND YEAR(date_created) = $selected_year
-                        GROUP BY label, medicine_name";
-                $report_label = 'Monthly';
-                break;
+    case 'month':
+        $sql = "SELECT CONCAT(YEAR(date_created), '-', MONTHNAME(date_created)) AS label,
+                medicine_name,
+                COUNT(medicine_name) AS total_medicine,
+                SUM(quantity) AS total_quantity
+                FROM medicine
+                WHERE role = '1' AND YEAR(date_created) = ?
+                GROUP BY label, medicine_name";
+        $report_label = 'Monthly';
+        break;
 
-            case 'year':
-                $sql = "SELECT CONCAT(YEAR(date_created)) AS label,
-                        medicine_name,
-                        COUNT(medicine_name) AS total_medicine,
-                        SUM(quantity) AS total_quantity
-                        FROM medicine
-                        WHERE role = '1' AND YEAR(date_created) = $selected_year
-                        GROUP BY label, medicine_name";
-                $report_label = 'Yearly';
-                break;
+    case 'year':
+        $sql = "SELECT CONCAT(YEAR(date_created)) AS label,
+                medicine_name,
+                COUNT(medicine_name) AS total_medicine,
+                SUM(quantity) AS total_quantity
+                FROM medicine
+                WHERE role = '1' AND YEAR(date_created) = ?
+                GROUP BY label, medicine_name";
+        $report_label = 'Yearly';
+        break;
 
-            default:
-                echo "Invalid report type selection.";
-                exit;
-        }
+    default:
+        echo "Invalid report type selection.";
+        exit;
+}
 
-        $result = $conn->query($sql);
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $selected_year);
+$stmt->execute();
+$result = $stmt->get_result();
 ?>
-<table class="clinic-table">
+<form class="clinic-form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <select id="tableSelect" name="report_type">
+        <option value="week" <?php echo $report_type === 'week' ? 'selected' : ''; ?>>Weekly</option>
+        <option value="month" <?php echo $report_type === 'month' ? 'selected' : ''; ?>>Monthly</option>
+        <option value="year" <?php echo $report_type === 'year' ? 'selected' : ''; ?>>Yearly</option>
+    </select>
+
+    <select id="yearSelect" name="selected_year">
+        <option value="2023" <?php echo $selected_year === '2023' ? 'selected' : ''; ?>>2023</option>
+        <option value="2024" <?php echo $selected_year === '2024' ? 'selected' : ''; ?>>2024</option>
+        <option value="2025" <?php echo $selected_year === '2025' ? 'selected' : ''; ?>>2025</option>
+    </select>
+
+    <button type="submit">Generate Report</button>
+    <button id="printButton">Print Report</button>
+
+</form>
+
+<table class="clinic-table" id="clinicTable">
         <thead>
             <tr>
                 <th><?php echo $report_label; ?></th>
@@ -306,25 +330,58 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         </tbody>
     </table>
 
-    <?php
-        }
-    }
-    ?>
-    <form class="clinic-form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <select id="tableSelect" name="report_type">
-            <option value="week">Week</option>
-            <option value="month">Month</option>
-            <option value="year">Year</option>
-        </select>
+    <script>
+  document.getElementById("printButton").addEventListener("click", function () {
+    var table = document.getElementById("clinicTable");
+    var printWindow = window.open('', '', 'width=800,height=600');
+    printWindow.document.open();
+    
+    printWindow.document.write('<table style="margin: 0 auto;"><tr>');
 
-        <select id="yearSelect" name="selected_year">
-            <option value="2023" <?php echo $selected_year === '2023' ? 'selected' : ''; ?>>2023</option>
-            <option value="2024" <?php echo $selected_year === '2024' ? 'selected' : ''; ?>>2024</option>
-            <option value="2025" <?php echo $selected_year === '2025' ? 'selected' : ''; ?>>2025</option>
-        </select>
+    // Create a div to hold the logo image with center-aligned cell
+    printWindow.document.write('<td style="text-align: center;">');
+    printWindow.document.write('<div style="text-align: center;"><img src="assets/images/dwcl.png" alt="Logo" width="100" height="100"></div>');
+    printWindow.document.write('</td>');
 
-        <button type="submit">Generate Report</button>
-    </form>
+    // Add another cell for the text beside the image
+    printWindow.document.write('<td style="text-align: center;">');
+    printWindow.document.write('<div style="text-align: center;"><b>HEALTH SERVICE UNIT - GS and JHS Department</b></div>');
+    printWindow.document.write('</td>');
+
+    // Close the row and start a new row for your table content
+    printWindow.document.write('</tr></table>');
+
+    printWindow.document.write('<br><br>');
+// Create the table for your content
+    printWindow.document.write('<style>');
+    printWindow.document.write('table { margin: 0 auto; }');
+    printWindow.document.write('tr.header { background-color: #f2f2f2; text-align: center; }');
+    printWindow.document.write('table.special-table { border-collapse: collapse; width: 80%; max-width: 800px; border: 1px solid #ccc; }');
+    printWindow.document.write('table.special-table th, table.special-table td { border: 2px solid #ccc; padding: 10px; text-align: left; }');
+    printWindow.document.write('table.special-table th { background-color: #333; color: #fff; }');
+    printWindow.document.write('</style>');
+
+// Create a row to hold your table
+printWindow.document.write('<tr>');
+printWindow.document.write('<td>' + table.outerHTML.replace('<table', '<table class="special-table"') + '</td>');
+printWindow.document.write('</tr>');
+
+
+    // Close the HTML document
+    printWindow.document.write('</body></html>');
+    printWindow.document.close();
+    
+    // Trigger the print dialog
+    printWindow.print();
+    printWindow.close();
+  });
+</script>
+
+
+
+  
+
+
 
     <!-- Include your JavaScript dependencies here -->
     <script src="assets/plugins/popper.min.js"></script>
